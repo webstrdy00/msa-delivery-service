8abc34f609f4cba6be2dbfad0dab368106254e33
webstrdy00 <les_roka22@naver.com>
2025-01-23 02:14:18 +0900

    init commit

---
+++ coupon/coupon-common/src/main/java/com/example/coupon/couponcommon/CouponCommonApplication.java
@@ -0,0 +1,13 @@
+package com.example.couponcommon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CouponCommonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CouponCommonApplication.class, args);
+	}
+
+}
---
+++ coupon/coupon-common/src/test/java/com/example/couponcommon/CouponCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.couponcommon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouponCommonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
